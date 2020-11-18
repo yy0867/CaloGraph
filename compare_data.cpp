b4096c29a38bafa2b76c0b515bfe0e3c471c8ab8
Jaeha Lee <jaehawilly@naver.com>
2020-11-18 17:04:02 +0900

    compare

---
+++ compare_data.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//#include <fstream>
+//#include "compare_data.h"
+//using namespace std;
+//
+//void printMessage()
+//{
+//	ifstream file;
+//	string username;
+//	
+//	cin >> username;
+//	username += ".txt";
+//	file.open(username);
+//	if (file.is_open()) {
+//		while (!file.eof()) {
+//			char buf[256];
+//			file.getline(buf, 256);
+//			cout << 
+//			
+//		}
+//	}
+//	file.close();
+//
+//}
