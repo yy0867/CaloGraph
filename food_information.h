30f931bcb8aab4b2eb4df16cfa76d8191f904821
Kim EunSung <dmstjd365@naver.com>
2020-11-20 18:20:21 +0900

    replace food

--- food_information.h
+++ food_information.h
@@ -2,35 +2,66 @@
 #include <vector>
 #include <string>
 using namespace std;
-//음식 탐색, 목록 추가, 목록에서 제거 필요
-class Foods {
-public:
-	Foods(string one_line);
-	string get_one_info();		// name car pro fat cal 한줄 반환
 
-	string get_name() const { return food_name; }
-	double get_carbo() const { return carbo; }
-	double get_protein() const { return protein; }
-	double get_fat() const { return fat; }
-	double get_calorie() const { return calorie; }
+// 탄, 단, 지, 칼로리
+class Nutrition
+{
+public:
+	Nutrition();
+	Nutrition(int carbo, int protein, int fat, int calorie);
+
+	int getCarbo() const { return carbo; }
+	int getProtein() const { return protein; }
+	int getFat() const { return fat; }
+	int getCalorie() const { return calorie; }
+
+	void setCarbo(int carbo) { this->carbo = carbo; }
+	void setProtein(int protein) { this->protein = protein; }
+	void setFat(int fat) { this->fat = fat; }
+	void setCalorie(int calorie) { this->calorie = calorie; }
+
+	void addCarbo(int carbo) { this->carbo += carbo; }
+	void addProtein(int protein) { this->protein += protein; }
+	void addFat(int fat) { this->fat += fat; }
+	void addCalorie(int calorie) { this->calorie += calorie; }
 
 private:
-	string food_name;
-	double carbo;
-	double protein;
-	double fat;
-	double calorie;
+	int carbo;
+	int protein;
+	int fat;
+	int calorie;	
+};
+
+//음식 탐색, 목록 추가, 목록에서 제거 필요
+class Food {
+public:
+	Food(wstring name, int carbo, int protein,
+		int fat, int calorie);
+	Food(wstring one_line);
+	wstring get_one_info();		// name car pro fat cal 한줄 반환
+
+	wstring get_name() const { return food_name; }
+	int getCarbo() const { return nutrition.getCarbo(); }
+	int getProtein() const { return nutrition.getProtein(); }
+	int getFat() const { return nutrition.getFat(); }
+	int getCalorie() const { return nutrition.getCalorie(); }
+
+private:
+	wstring food_name;
+	Nutrition nutrition;
 };
 
 class Foods_info {
 public:
 	Foods_info();
-	void add_food(Foods source);
+	void add_food(Food source);
 	void print_foods_info();
 
 	void read_from_file();
 	void write_on_file();
 
+	size_t getSize() const { return foods.size(); }
+
 private:
-	vector<Foods> foods;
+	vector<Food> foods;
 };
--- main_shapes.cpp
+++ main_shapes.cpp
@@ -2,6 +2,7 @@
 //#include "Simple_window.h"
 #include "food_information.h"
 #include "Information.h"
+#include "DatePerNutrition.h"
 #include <iostream>
 #include <vector>
 #include <conio.h>
@@ -13,7 +14,24 @@ void print()
 	cout << "now print graph" << endl;
 }
 
-int main() {
+int main()
+{
+	/*wcout.imbue(locale("ko_KR.UTF-8"));
+	wcin.imbue(locale("ko_KR.UTF-8"));
+	locale::global(locale("ko_KR.UTF-8"));*/
+
+	/*vector<Food> foodList(150);
+	loadFoodList(foodList);
+	cout << foodList.size();
+
+	for (int i = 0; i < foodList.size(); i++) {
+		cout << foodList[i].get_name() << " ";
+		cout << foodList[i].getCarbo() << " ";
+		cout << foodList[i].getProtein() << " ";
+		cout << foodList[i].getFat() << " ";
+		cout << foodList[i].getCalorie() << endl;
+	}*/
+
 	Foods_info xaeha;
 	xaeha.print_foods_info();
 	/*
@@ -32,5 +50,6 @@ int main() {
 		if (t == 27) break;
 	}
 	*/
+
 	return 0;
 }
