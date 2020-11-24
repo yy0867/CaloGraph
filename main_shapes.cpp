8e7a98656a11e6d10fe8fa91427b5ea1eb12ee44
yy0867 <yy0867@naver.com>
2020-11-24 19:13:51 +0900

    temp

--- main_shapes.cpp
+++ main_shapes.cpp
@@ -18,19 +18,20 @@ void print()
 
 int main()
 {
-	/*system(" mode  con lines=50   cols=130 ");
+	
+
+	USES_CONVERSION;
+	// string, cout, cin으로 한글 안나오면 추가해줄 것 
+
+	system(" mode  con lines=50   cols=130 ");
 	init_calo_ui();
 
 	while (1) {
 		print_edge();
 		print_main_menu();
-	}*/
-
-	USES_CONVERSION;
-	// string, cout, cin으로 한글 안나오면 추가해줄 것 
-
-
-	Foods_info xaeha;
+	}
+	
+	/*Foods_info xaeha;
 
 	Food new_food(L"이재하", 100, 100, 100, 100);
 	xaeha.add_food(new_food);
@@ -38,7 +39,7 @@ int main()
 	xaeha.print_foods_info();
 	cout << endl << endl << endl;
 
-	cout << "xaeha 등장 위치: " << xaeha.is_exist(L"이재하");
+	cout << "xaeha 등장 위치: " << xaeha.is_exist(L"이재하");*/
 
 	return 0;
 }
